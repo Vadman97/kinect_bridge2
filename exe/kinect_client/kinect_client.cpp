6a2bba7d84483e249f8f9c09ad1596133b9df6d4
Michael Tsang <tsangm@usc.edu>
2015-12-02 19:41:06 -0800

    ...

--- exe/kinect_client/kinect_client.cpp
+++ exe/kinect_client/kinect_client.cpp
@@ -158,8 +158,8 @@ public:
                 auto const & joints_msg = body_msg.joints_;
                 auto & ros_joints_msg = ros_body_msg.joints;
 
-				ros_body_msg.lean.x = body_msg.lean_.x;
-				ros_body_msg.lean.y = body_msg.lean_.y;
+				ros_body_msg.lean.x = static_cast<float>(body_msg.lean_.x);
+				ros_body_msg.lean.y = static_cast<float>(body_msg.lean_.y);
 
 				std::cout << "LX: " << ros_body_msg.lean.x << " LY: " << ros_body_msg.lean.y << std::endl;
 
--- include/kinect_common/kinect_device.h
+++ include/kinect_common/kinect_device.h
@@ -1062,6 +1062,8 @@ public:
             if( FAILED( body->get_HandRightState( reinterpret_cast<HandState *>( &body_msg.hand_state_right_ ) ) ) ) throw KinectException( "Failed to get right hand state" );
             if( FAILED( body->get_TrackingId( &body_msg.tracking_id_ ) ) ) throw KinectException( "Failed to get body tracking ID" );
 
+			body_msg.lean_.x = static_cast<float>(.5);
+			body_msg.lean_.y = static_cast<float>(.75);
             // only fill out joint info if body is tracked
             if( body_msg.is_tracked_ )
             {
@@ -1103,7 +1105,7 @@ public:
                 }
             }
 
-			std::cout << "bIdx: " << bodies_idx << "bLean: " << body_msg.lean_.x << " M:" << &body_msg << std::endl;
+			std::cout << "bIdx: " << bodies_idx << "bLeanX: " << body_msg.lean_.x << " M:" << &body_msg << std::endl;
             payload.emplace_back( std::move( body_msg ) );
         }
     }
